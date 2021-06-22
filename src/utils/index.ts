1a6443432815bdcf2a64c9f0d139ca722a1b16b3
陳栄超 <48024543+chenmiaochao@users.noreply.github.com>
2021-06-22 14:35:38 +0900

    chore: 添加ts类型定义

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,24 +1,32 @@
 import { useEffect, useState } from "react";
 
 // 在函数里，改变传入对象是不好的
-export const isFalsy = (value) => (value === 0 ? false : !value);
+export const isFalsy = (value: any) => (value === 0 ? false : !value);
 //!!两个感叹号 对反求反 转变为布尔值
 
-export const cleanObject = (object) => {
+export const cleanObject = (object: object) => {
   // Object.assign({}, object)
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    //@ts-ignore
     const value = result[key];
     //如果value为false，包括undefined unll
     //排除传入值为0的情况
     if (isFalsy(value)) {
+      //@ts-ignore
       delete result[key];
     }
   });
   return result;
 };
 
-export const useDebounce = (value, delay) => {
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value: any, delay?: number) => {
   const [debounceValue, setDebonceValue] = useState(value);
   //每次在value变化以后，设置一个定时器
   useEffect(() => {
