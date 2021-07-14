38d881da631da7c2be07ef7b93708151dc33e12d
陳栄超 <48024543+chenmiaochao@users.noreply.github.com>
2021-07-14 11:31:55 +0900

    add : ts 用法

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -43,8 +43,53 @@ export const http = async (
   });
 };
 
+//js中的typeof是在runtime中运行的
+//ts中的typeof是在静态环境运行的
+// return typeof 1 === 'number'
+
 export const useHttp = () => {
   const { user } = useAuth();
+  // utility type用法：用泛型给它传入一个其它类型，然后utility type对这个类型惊醒某种操作
   return (...[endpoint, config]: Parameters<typeof http>) =>
     http(endpoint, { ...config, token: user?.token });
 };
+
+// 联合类型
+// let myFavoriteNunber: string | number;
+// myFavoriteNunber = "seven";
+// myFavoriteNunber = 7;
+//'myFavoriteNunber' is assigned a value but never used.eslint@typescript-eslint/no-unused-vars
+// myFavoriteNunber = {}
+// let jackFavouriteNumber: string | number;
+
+//类型别名
+// type FavouriteNumber = string | number;
+// let roseFavouriteNumber: FavouriteNumber = "6";
+
+//类型别名在很多情况下可以和interface呼唤
+// interface Person {
+//   name: string;
+// }
+// type Person = {name: string}
+// const xiaoming: Person = { name: "xiaoming" };
+
+//类型别名在这种情况下没法替代type
+type FavouriteNumber = string | number;
+let roseFavouriteNumber: FavouriteNumber = 6;
+
+//interface 也没法实现Utility type
+type Person = {
+  name: string;
+  age: number;
+};
+
+const xiaoming: Partial<Person> = { age: 8 };
+const shenMiRen: Omit<Person, "name" | "age"> = {};
+type PersonKeys = keyof Person;
+type PersonOnlyName = Pick<Person, "name">;
+
+//Partial 的实现
+type Partial<T> = {
+  [P in keyof T]?: T[P];
+};
+// 这里的？ 使所有的key都变成了可选 从而实现了Partial
