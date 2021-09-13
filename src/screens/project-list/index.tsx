d49e83cd318b30f5560ec4fd330c4adcc466a8da
陳栄超 <48024543+chenmiaochao@users.noreply.github.com>
2021-09-13 11:48:55 +0900

    优化项目列表页面

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from "react";
-import { SearchPanel } from "./search-panel";
-import { List } from "./list";
-import * as qs from "qs";
-import { cleanObject, useDebounce, useMount } from "utils";
-import { useHttp } from "utils/http";
+import { useEffect, useState } from 'react';
+import { SearchPanel } from './search-panel';
+import { List } from './list';
+import * as qs from 'qs';
+import { cleanObject, useDebounce, useMount } from 'utils';
+import { useHttp } from 'utils/http';
+import styled from '@emotion/styled';
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -11,24 +12,28 @@ export const ProjectListScreen = () => {
   const [users, setUsers] = useState([]);
 
   const [param, setParam] = useState({
-    name: "",
-    personId: "",
+    name: '',
+    personId: '',
   });
   const debouncedParam = useDebounce(param, 200);
   const [list, setList] = useState([]);
   const client = useHttp();
 
   useEffect(() => {
-    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
+    client('projects', { data: cleanObject(debouncedParam) }).then(setList);
   }, [debouncedParam]);
 
   useMount(() => {
-    client("users").then(setUsers);
+    client('users').then(setUsers);
   });
   return (
-    <div>
+    <Container>
       <SearchPanel param={param} setParam={setParam} users={users} />
       <List list={list} users={users} />
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  padding: 3.2rem;
+`;
--- src/screens/project-list/search-panel.tsx
+++ src/screens/project-list/search-panel.tsx
@@ -1,4 +1,6 @@
-import { Input, Select } from "antd";
+/** @jsxImportSource @emotion/react */
+import { jsx } from '@emotion/react';
+import { Form, Input, Select } from 'antd';
 
 export interface User {
   id: string;
@@ -15,38 +17,43 @@ interface SearchPanelProps {
     name: string;
     personId: string;
   };
-  setParam: (param: SearchPanelProps["param"]) => void;
+  setParam: (param: SearchPanelProps['param']) => void;
 }
 
 export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
   return (
-    <form>
-      <Input
-        type="text"
-        value={param.name}
-        onChange={(evt) =>
-          setParam({
-            ...param,
-            name: evt.target.value,
-          })
-        }
-      />
-      <Select
-        value={param.personId}
-        onChange={(value) =>
-          setParam({
-            ...param,
-            personId: value,
-          })
-        }
-      >
-        <Select.Option value={""}>负责人</Select.Option>
-        {users.map((user) => (
-          <Select.Option key={user.id} value={user.id}>
-            {user.name}
-          </Select.Option>
-        ))}
-      </Select>
-    </form>
+    <Form css={{ marginBottom: '2rem' }} layout={'inline'}>
+      <Form.Item>
+        <Input
+          placeholder={'项目名'}
+          type="text"
+          value={param.name}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              name: evt.target.value,
+            })
+          }
+        />
+      </Form.Item>
+      <Form.Item>
+        <Select
+          value={param.personId}
+          onChange={(value) =>
+            setParam({
+              ...param,
+              personId: value,
+            })
+          }
+        >
+          <Select.Option value={''}>负责人</Select.Option>
+          {users.map((user) => (
+            <Select.Option key={user.id} value={user.id}>
+              {user.name}
+            </Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
+    </Form>
   );
 };
