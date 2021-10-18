a8cab404359095c502d88871910db3a53ae18b8f
陳栄超 <48024543+chenmiaochao@users.noreply.github.com>
2021-10-18 21:54:30 +0900

    feat: kanban 和 epic路由

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,3 +1,5 @@
 export { ProjectListScreen } from './project-list';
 export type { User } from './project-list/search-panel';
 export { ProjectScreen } from './project';
+export { KanbanScreen } from './kanban';
+export { EpicScreen } from './epic';
--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { Routes, Route, Navigate, useNavigate } from 'react-router';
+import { Link } from 'react-router-dom';
+import { EpicScreen, KanbanScreen } from 'screens';
 
 export const ProjectScreen = () => {
-  return <h1>ProjectScreen</h1>;
+  return (
+    <div>
+      <h1>ProjectScreen</h1>
+      <Link to={'kanban'}>看板</Link>
+      <Link to={'epic'}>任务组</Link>
+      <Routes>
+        {/* /projects/:projectId/kanban */}
+        <Route path={'/kanban'} element={<KanbanScreen />}></Route>
+        {/* /projects/:projectId/epic */}
+        <Route path={'/epic'} element={<EpicScreen />}></Route>
+        <Navigate to={window.location.pathname + '/kanban'} />
+      </Routes>
+    </div>
+  );
 };
