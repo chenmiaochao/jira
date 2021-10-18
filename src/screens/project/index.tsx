6e0ed149e30a6dc6085791e0ea46666138b23ded
陳栄超 <48024543+chenmiaochao@users.noreply.github.com>
2021-10-18 22:02:46 +0900

    fix: 重新安装react-router

--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -10,10 +10,10 @@ export const ProjectScreen = () => {
       <Link to={'kanban'}>看板</Link>
       <Link to={'epic'}>任务组</Link>
       <Routes>
-        {/* /projects/:projectId/kanban */}
-        <Route path={'/kanban'} element={<KanbanScreen />}></Route>
-        {/* /projects/:projectId/epic */}
-        <Route path={'/epic'} element={<EpicScreen />}></Route>
+        {/*projects/:projectId/kanban*/}
+        <Route path={'/kanban'} element={<KanbanScreen />} />
+        {/*projects/:projectId/epic*/}
+        <Route path={'/epic'} element={<EpicScreen />} />
         <Navigate to={window.location.pathname + '/kanban'} />
       </Routes>
     </div>
